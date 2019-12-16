4da44563df063927bf17fc72558e63438334bba2
mattkduran <19656092+mattkduran@users.noreply.github.com>
2019-12-16 11:52:18 -0800

    Menu functionality
    Changed format of program to include a menu, check for existing files moved to header.py

--- ___main____.py
+++ ___main____.py
@@ -4,14 +4,5 @@ import os
 
 
 def main():
-    checkValue = checkExists()
-    if(checkValue == 5):
-        PPSmasterFrame = loadMasterPPS(PPSsource)
-        WLWVMasterFrame = loadMasterWLWV(WLWVsource)
-        looper(PPSmasterFrame, WLWVMasterFrame)
-        destroy(PPSmasterFrame, WLWVMasterFrame)
-        print("\nJob completed. Quitting...\n")
-    else:
-        print("Needed files are missing, please confirm that they have been correctly saved.\n")
-        print("Quitting...")
+    menu()
     return 0
